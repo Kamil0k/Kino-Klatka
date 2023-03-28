625b7c986cf957bd2055c4457364b418f2cfdae8
Kamil Porada <kamil@macbook-air-kamil.home>
2023-03-28 12:03:11 +0200

    finish navigation

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import Brand from './Brand'
-import { IconMenu2 } from '@tabler/icons-react'
+import { IconMenu2, IconX } from '@tabler/icons-react'
 
 import './MainNavigation.css'
 
@@ -18,23 +18,24 @@ function HamburgerNavigation() {
 
 	return (
 		<>
-			<header className='header'>
+			<div className='nav'>
 				<Brand />
 				<nav className='nav-mobile'>
-					<IconMenu2 onClick={handleHamburgerMenu} className='nav-mobile__icon' />
+					{!isMenuVisible ? <IconMenu2 onClick={handleHamburgerMenu} className='nav-mobile__icon' /> : <IconX onClick={handleHamburgerMenu} className='nav-mobile__icon' />}
+					
 					<ul className={`nav-mobile__list ` + `${isMenuVisible ? 'show' : 'hide'}`}>
 						<li>
-							<NavLink onClick={handleLinks} className='nav-mobile__list-item'>
+							<NavLink onClick={handleLinks} className='nav-item'>
 								strona główna
 							</NavLink>
 						</li>
 						<li>
-							<NavLink onClick={handleLinks} className='nav-mobile__list-item'>
+							<NavLink onClick={handleLinks} className='nav-item'>
 								repertuar
 							</NavLink>
 						</li>
 						<li>
-							<NavLink onClick={handleLinks} className='nav-mobile__list-item'>
+							<NavLink onClick={handleLinks} className='nav-item'>
 								zaloguj się
 							</NavLink>
 						</li>
@@ -43,17 +44,17 @@ function HamburgerNavigation() {
 				<nav className='nav-desktop'>
 					<ul className='nav-desktop__list'>
 						<li>
-							<NavLink className='nav-desktop__list-item'>strona główna</NavLink>
+							<NavLink className='nav-item'>strona główna</NavLink>
 						</li>
 						<li>
-							<NavLink className='nav-desktop__list-item'>repertuar</NavLink>
+							<NavLink className='nav-item'>repertuar</NavLink>
 						</li>
 						<li>
-							<NavLink className='nav-desktop__list-item'>zaloguj się</NavLink>
+							<NavLink className='nav-item'>zaloguj się</NavLink>
 						</li>
 					</ul>
 				</nav>
-			</header>
+			</div>
 		</>
 	)
 }
