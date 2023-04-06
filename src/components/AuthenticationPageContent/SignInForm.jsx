c6eab5ca4cd9a89a98736576f4046605b19d5b88
Kamil0k <kamil.porada@onet.pl>
2023-04-06 21:43:57 +0200

    validation sign in and sign up form

--- src/components/AuthenticationPageContent/SignInForm.jsx
+++ src/components/AuthenticationPageContent/SignInForm.jsx
@@ -1,15 +1,56 @@
 import './SignInForm.css'
 import Button from '../UI/Button'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const SignInForm = () => {
+	const [formData, setFormData] = useState({
+		email:'',
+		password:''
+	})
+
+	const [errors, setErrors] = useState({})
+
+	const handleChange = event => {
+		const name = event.target.id
+		const value = event.target.value
+		setFormData({ ...formData, [name]: value })
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		const validationErrors = validateFormData(formData)
+		setErrors(validationErrors)
+		if (Object.keys(validationErrors).length === 0) {
+			//logowanie do systemu
+		}
+	}
+
+	const validateFormData = data => {
+		const errors = {}
+		if (!data.email) {
+			errors.email = 'Podaj adres email!'
+		} else if (!/\S+@\S+\.\S+/.test(data.email)) {
+			errors.email = 'Niepoprawny adres email!'
+		}
+		if (!data.password) {
+			errors.password = 'Podaj hasło!'
+		} else if (data.password.length < 10) {
+			errors.password = 'Hasło musi się składać z 10 znaków!'
+		}
+		return errors
+	}
+
+
 	return (
 		<>
-			<form action='post' className='form-signin'>
+			<form onSubmit={handleSubmit} className='form-signin'>
 				<i className='fa-regular fa-user form-signin__icon'></i>
 				<h3 className='form-signin__title'>Zaloguj się!</h3>
-				<input type='email' id='email' placeholder='Email' required className='form-signin__input' />
-				<input type='password' placeholder='Hasło' required className='form-signin__input' />
+				<input type='email' id='email' placeholder='Email' className='form-signin__input' onChange={handleChange}/>
+				{errors.email && <span className='form-signup__error'>{errors.email}</span>}
+				<input type='password' id='password' placeholder='Hasło' className='form-signin__input' onChange={handleChange}/>
+				{errors.password && <span className='form-signup__error'>{errors.password}</span>}
 				<div className='form-signin__check'>
 					<input type='checkbox' id='employee-checkbox' name='employee' className='form-signin__check-input' />
 					<label className='form-signin__check-label' forhtml='employee-checkbox'>
--- src/components/AuthenticationPageContent/SignUpForm.jsx
+++ src/components/AuthenticationPageContent/SignUpForm.jsx
@@ -3,24 +3,110 @@ import './SignUpForm.css'
 import Button from '../UI/Button'
 
 const SignUpForm = () => {
+	const [formData, setFormData] = useState({
+		name: '',
+		surname: '',
+		idOfEmployee: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	})
 
-    const [isEmployee, setIsEmployee] = useState(false)
+	const [errors, setErrors] = useState({})
+
+	const [isEmployee, setIsEmployee] = useState(false)
 
 	const employeeHandler = () => {
-        setIsEmployee(!isEmployee)
+		setIsEmployee(!isEmployee)
+	}
+
+	const handleChange = event => {
+		const name = event.target.id
+		const value = event.target.value
+		setFormData({ ...formData, [name]: value })
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		const validationErrors = validateFormData(formData)
+		setErrors(validationErrors)
+		if (Object.keys(validationErrors).length === 0) {
+			//zapis do bazy danych
+		}
+	}
+
+	const validateFormData = data => {
+		const errors = {}
+		if (formData.name.length === 0) {
+			errors.name = 'Podaj imię!'
+		}
+		if (!data.surname) {
+			errors.surname = 'Podaj nazwisko!'
+		}
+		if (isEmployee) {
+			if (!data.idOfEmployee) {
+				errors.idOfEmployee = 'Podaj identfikator!'
+			} else if (data.idOfEmployee.length !== 12) {
+				errors.idOfEmployee = 'Identyfikator musi posiadać 12 znaków!'
+			} else if (parseInt(data.idOfEmployee.charAt(data.idOfEmployee.length - 1)) % 5 !== 0) {
+				errors.idOfEmployee = 'Niepoprawny identyfikator!'
+			}
+		}
+		if (!data.email) {
+			errors.email = 'Podaj adres email!'
+		} else if (!/\S+@\S+\.\S+/.test(data.email)) {
+			errors.email = 'Niepoprawny adres email!'
+		}
+		if (!data.password) {
+			errors.password = 'Podaj hasło!'
+		} else if (data.password.length < 10) {
+			errors.password = 'Hasło musi się składać z 10 znaków!'
+		}
+		if (!data.confirmPassword) {
+			errors.confirmPassword = 'Powtórz hasło!'
+		} else if (data.password !== data.confirmPassword) {
+			errors.confirmPassword = 'Hasła nie pasują do siebie!'
+		}
+		return errors
 	}
 
 	return (
 		<>
-			<form action='post' className='form-signup'>
+			<form onSubmit={handleSubmit} className='form-signup'>
 				<i className='fa-regular fa-user form-signup__icon'></i>
 				<h3 className='form-signup__title'>Zarejestruj się!</h3>
-				<input type='text' id='name' placeholder='Imię' required className='form-signup__input' />
-				<input type='text' id='surname' placeholder='Nazwisko' required className='form-signup__input' />
-                {isEmployee && <input type='text' id='idOfEmployee' placeholder='ID pracownika' required className='form-signup__input' />}
-				<input type='email' id='email' placeholder='Email' required className='form-signup__input' />
-				<input type='password' placeholder='Hasło' required className='form-signup__input' />
-				<input type='password' placeholder='Powtórz hasło' required className='form-signup__input' />
+				<input type='text' id='name' placeholder='Imię' className='form-signup__input' onChange={handleChange} />
+				{errors.name && <span className='form-signup__error'>{errors.name}</span>}
+				<input type='text' id='surname' placeholder='Nazwisko' className='form-signup__input' onChange={handleChange} />
+				{errors.surname && <span className='form-signup__error'>{errors.surname}</span>}
+				{isEmployee && (
+					<input
+						type='text'
+						id='idOfEmployee'
+						placeholder='ID pracownika'
+						className='form-signup__input'
+						onChange={handleChange}
+					/>
+				)}
+				{errors.idOfEmployee && isEmployee && <span className='form-signup__error'>{errors.idOfEmployee}</span>}
+				<input type='email' id='email' placeholder='Email' className='form-signup__input' onChange={handleChange} />
+				{errors.email && <span className='form-signup__error'>{errors.email}</span>}
+				<input
+					type='password'
+					id='password'
+					placeholder='Hasło'
+					className='form-signup__input'
+					onChange={handleChange}
+				/>
+				{errors.password && <span className='form-signup__error'>{errors.password}</span>}
+				<input
+					type='password'
+					id='confirmPassword'
+					placeholder='Powtórz hasło'
+					className='form-signup__input'
+					onChange={handleChange}
+				/>
+				{errors.confirmPassword && <span className='form-signup__error'>{errors.confirmPassword}</span>}
 				<div className='form-signup__check'>
 					<input
 						type='checkbox'
